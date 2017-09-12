8ef5fde525488267255387d2f1b266ed5814af76
mbenevides <30814045+mbenevides@users.noreply.github.com>
2017-09-12 19:55:12 -0300

    Update jogovelha.py

--- docs/docs/src/jogovelha.py
+++ docs/docs/src/jogovelha.py
@@ -1,1 +1,15 @@
-
+def inicializar( ):
+    tab = []
+    for i in range(3):
+      linha = []
+      for j in range (3):
+        linha.append(".")
+       tab.append(linha)
+     return tab
+  
+  def main( ):
+    jogo = inicializar( )
+    print (jogo)
+    
+    if_name_== "_main_":
+      main()
